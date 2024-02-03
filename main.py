7ff942da01960635318e747a22133564d7b89512
alwayswannafeedPC <vladyslavvelbovets@knu.ua>
2024-02-03 14:40:35 +0200

    Viva Mech-mat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+def VivaMechMat():
+    return "Viva Mech-mat faculty!!!"
 def Hi():
-  return "Hello, world"
-for i in range(10):
-    print(Hi())
+    return "Hello, world!"
+#for i in range(10):
+#    print(Hi())
+print(VivaMechMat())
